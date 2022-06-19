5c34b92eb39c831e6acaab4ea51cbe589fca6574
SuzdalevDenis <suzdalev.denis@mail.ru>
2022-06-19 17:32:30 +0300

    initial commit

---
+++ 3_1.py
@@ -0,0 +1,6 @@
+text = "--".join(' hello World '.split(' '))
+print(text)
+
+text = " h o m e w o r k "
+text = text.replace(" ", "-")
+print(text)
--- 3_2.py
+++
@@ -1,6 +0,0 @@
-text = "--".join(' hello World '.split(' '))
-print(text)
-
-text = " h o m e w o r k "
-text = text.replace(" ", "-")
-print(text)
