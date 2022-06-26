47a0f068da4eeaf8bad7b217abbd7db107b12b06
SuzdalevDenis <suzdalev.denis@mail.ru>
2022-06-26 17:32:44 +0300

    initial commit

---
+++ 5_2.py
@@ -0,0 +1,11 @@
+numb = float(input('Число:'))
+simb = input('Символ:')
+numb2 = float(input('Число:'))
+if simb == '+':
+  print(numb + numb2)
+elif simb == '-':
+  print(numb - numb2)
+elif simb == '*':
+  print(numb * numb2)
+elif simb == '/':
+  print(numb / numb2)
